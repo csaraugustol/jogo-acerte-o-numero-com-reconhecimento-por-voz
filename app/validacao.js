3258837c9d5f92d19fcee08a7c2c45d7519d363c
csaraugustol <cesar.leitejm@hotmail.com>
2022-09-02 11:23:42 -0300

    Ajustes finais

--- app/validacao.js
+++ app/validacao.js
@@ -1,11 +1,26 @@
 function verificaSeOChuteEhUmValorValido(chute) {
+    console.log("chuite", chute === "game over");
     const numero = +chute;
+
+    if(chute === "game over") {
+        document.body.innerHTML = `
+        <button id="btn-dark-mode" style="display: none"><i class="fa-solid ${darkOrLigth}"></i></button>
+        <div class="container">
+        <h1 class="errou">Você disse</h1>
+        <h2 class="errou">"Game Over"</h2>
+        <h3>Clique no botão para reiniciar o jogo</h3>
+        <button id="jogar-novamente" class="btn-jogar-novamente">Jogar Novamente <i class="fa-solid fa-rotate-right"></i></button>
+        </div>
+        `;
+
+        return;
+    }
 
     if(chuteEhInvalido(numero)) {
         elementoChute.innerHTML += `<div class="errou">Valor inválido: Fale um número.</div>`
         return;
     }
-
+    
     if(numeroMaiorOuMenorValorPermiido(numero)) {
         elementoChute.innerHTML += `<div class="errou">Número inválido: Fale um número entre <span>${menorValor}</span> e <span>${maiorValor}</span>.</div>`;
         return;
